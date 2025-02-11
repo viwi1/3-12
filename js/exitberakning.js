fd432bc435de0fb311690e752a5045e820684cb7
viwi1 <viktorwilson@me.com>
2025-02-11 19:49:36 +0100

    Update exitberakning.js

--- js/exitberakning.js
+++ js/exitberakning.js
@@ -1,5 +1,16 @@
 import { updateState, getState } from "./state.js";
 
+
+function uppdateraNuvarde() {
+    let nuvarde = document.getElementById("daligtNuvarde").checked ? 3000000 : 6855837;
+    updateState("exitVarde", nuvarde);
+    document.getElementById("nuvarde").textContent = formatNumber(nuvarde);
+    uppdateraBeräkningar();
+}
+
+document.getElementById("daligtNuvarde").addEventListener("change", uppdateraNuvarde);
+
+
 function uppdateraBeräkningar() {
     let multipel = parseFloat(document.getElementById("multipel").value);
     document.getElementById("multipelValue").textContent = multipel.toFixed(1);
